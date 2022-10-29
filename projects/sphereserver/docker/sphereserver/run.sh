e8fa872921b25ec0062d98479f64dbd8e5ba4ae9
cavefish <cavefish@gmail.com>
2022-10-29 21:58:32 +0200

    Add env variables for autoconfig

---
+++ projects/sphereserver/docker/sphereserver/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+sed -i 's/127.0.0.1/${HOSTNAME}/' sphere.ini
+sed -i 's/My Shard/${SHARDNAME}/' sphere.ini
+
+echo "Running ${SHARDNAME} sphere server in ${HOSTNAME}:2593 ..."
+
+./spheresvr
