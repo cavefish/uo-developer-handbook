52162d95869fd0bc792d981fd4d10a4ca47f9ab5
cavefish <cavefish@gmail.com>
2022-10-30 10:36:38 +0100

    add initialization scripts

--- projects/sphereserver/docker/sphereserver/run.sh
+++ projects/sphereserver/docker/sphereserver/run.sh
@@ -2,8 +2,22 @@
 
 set -e
 
+function initializeFile() {
+  if [ ! -f "$1" ]; then
+    cp empty.scp "$1"
+  fi
+}
+
 sed -i 's/127.0.0.1/${HOSTNAME}/' sphere.ini
 sed -i 's/My Shard/${SHARDNAME}/' sphere.ini
+sed -i 's/\/\/MulFiles=mul\//MulFiles=mul\//' sphere.ini
+
+
+initializeFile "save/spheredata.scp"
+initializeFile "save/spherestatics.scp"
+initializeFile "save/spheremultis.scp"
+initializeFile "save/sphereworld.scp"
+initializeFile "save/spherechars.scp"
 
 ls -la
 
